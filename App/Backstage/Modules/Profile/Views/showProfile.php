222df54f2b6f21bce0e6f631860b63d01360b845
Johanna Jato <johanna.jato@gmail.com>
2020-11-05 22:23:39 +0100

    fix: css3

--- App/Backstage/Modules/Profile/Views/showProfile.php
+++ App/Backstage/Modules/Profile/Views/showProfile.php
@@ -17,7 +17,6 @@
         <img src="/assets/img/profil.png" alt='photo de profil'>
         <h2><strong><?= $user['pseudo'] ?></strong></h2>
         <p><?= $city['name'] ?> (<?= $city['country'] ?>)</p>
-        <a href="/signout/"><button type="button" name="button" class="logout">Se déconnecter</button></a>
     </div>
     <div class="userInfoPic">
         <div>
@@ -60,4 +59,6 @@
         <?php
     }
     ?>
+
+    <button type="button" name="button" class="logout"><a href="/signout/">Se déconnecter</a></button>
 </section>
